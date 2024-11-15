80ede0e45258d16ea63c8e1fb2e6792ee0b854a9
Minn-Choi <min031106@dgu.ac.kr>
2024-11-15 21:06:11 +0900

    [Feature] #8 - 메인페이지 수정

--- src/page/MainPage/mainpage.jsx
+++ src/page/MainPage/mainpage.jsx
@@ -3,14 +3,18 @@ import StorePick from '../../components/MainPage/storePick';
 import CatePick from '../../components/MainPage/catePick';
 import MainHeader from '../../components/MainPage/header';
 import Banner from '../../components/MainPage/banner';
+import MainListPage from '../../components/MainPage/MainListPage';
 
 export const MainPage = () => {
   return (
-    <div className='MainPageWrapper'>
-      < MainHeader />
-      < StorePick />
-      < CatePick />
-      < Banner />
+    <div className="MainPageWrapper">
+      <MainHeader />
+      <StorePick />
+      <CatePick />
+      <Banner />
+      <div className='ListUp'>
+        <MainListPage />
+      </div>
     </div>
   );
 };
